bf4a8448a08f766a01fe48ce1c3f3351ae190e30
Janilabo <janilabo@gmail.com>
2025-07-26 22:07:04 +0300

    Upgrades! 🔧
    ..for TPointArrays, TRangeArrays and TBoxArrays. 😎

--- MiMU.pas
+++ MiMU.pas
@@ -340,19 +340,8 @@ type
     class function Create(const value: Integer = 0): TRange; overload; cdecl;
   end;
 
-function X(const arr: TPointArray): TIntegerArray; overload; cdecl;
-function X(const arr: TPointArray; const xVal: Integer): TIntegerArray; overload; cdecl;
-function X(const arr: TPointArray; const xVals: TIntegerArray): TIntegerArray; overload; cdecl;
-function Y(const arr: TPointArray): TIntegerArray; overload; cdecl;
-function Y(const arr: TPointArray; const yVal: Integer): TIntegerArray; overload; cdecl;
-function Y(const arr: TPointArray; const yVals: TIntegerArray): TIntegerArray; overload; cdecl;
-function X1(const arr: TBoxArray): TIntegerArray; overload; cdecl;
-function Y1(const arr: TBoxArray): TIntegerArray; overload; cdecl;
-function X2(const arr: TBoxArray): TIntegerArray; overload; cdecl;
-function Y2(const arr: TBoxArray): TIntegerArray; overload; cdecl;
-function Start(const arr: TRangeArray): TIntegerArray; overload; cdecl;
-function Stop(const arr: TRangeArray): TIntegerArray; overload; cdecl;
-  
+{$DEFINE IMPLEMENT}{$INCLUDE MiMU\config\Templates\D.inc}{$UNDEF IMPLEMENT} 
+ 
 implementation
 
 function MiMU_Version: Double; cdecl;
