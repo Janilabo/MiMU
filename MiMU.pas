69b1328f9c9cf72fc023eccd1d4ea35f8d2d5ece
Jani Lähdesmäki <janilabo@gmail.com>
2025-12-10 09:49:20 +0200

    Cleaning continues
    👷

--- MiMU.pas
+++ MiMU.pas
@@ -33,12 +33,13 @@
  unit MiMU;
 
 {$mode objfpc}
+{$H+}
 {$macro on}
 {$inline on}
 {$modeswitch advancedrecords}
 {$modeswitch arrayoperators}
 {$modeswitch typehelpers}
-{$H+}
+{$modeswitch generics}
 
 interface
 
@@ -74,14 +75,6 @@ generic function SetSize<T>(var A, B: specialize TArray<T>; const size: Integer 
 generic function SetSize<T>(var A, B, C: specialize TArray<T>; const size: Integer = 1): Integer; overload;
 generic function SetSize<T>(var A, B, C, D: specialize TArray<T>; const size: Integer = 1): Integer; overload;
 generic function Trade<T>(var A, B: T): Boolean; overload;
-
-function RandomB: Boolean; overload; 
-function RandomBit: Byte; overload; 
-function RandomSample(const x: Integer; const size: Integer): TIntegerArray; overload; 
-function RandomMean(const x: Integer; const sample: Integer = 10): Integer; overload; 
-function Random2(const x: Integer; const r: Integer = 2): Integer; overload; 
-function RandomMax(const x: Integer; const r: Integer = 2): Integer; overload; 
-function RandomMin(const x: Integer; const r: Integer = 2): Integer; overload;
 
 function Swop(var A, B: Integer; const oAscending: Boolean = True): Boolean; overload;
 function Swop(var A, B: Int64; const oAscending: Boolean = True): Boolean; overload;
@@ -297,85 +290,6 @@ begin
     B := C;
   end;
 end;
-
-function RandomB: Boolean; overload; 
-begin
-  Result := (Random(2) = 1);
-end;
-
-function RandomBit: Byte; overload; 
-begin
-  Result := Random(2);
-end;
-
-function RandomSample(const x: Integer; const size: Integer): TIntegerArray; overload; 
-var
-  i: Integer;
-begin
-  SetLength(Result, size);
-  for i := 0 to High(Result) do
-    Result[i] := Random(x);
-end;
-
-function RandomMean(const x: Integer; const sample: Integer = 10): Integer; overload; 
-begin
-  Result := Round(RandomSample(x, sample).Mean);
-end;
-
-function Random2(const x: Integer; const r: Integer = 2): Integer; overload; 
-var
-  i: Integer;
-begin
-  for i := 1 to Random(r + 1) do
-    Random(x);
-  Result := Random(x);
-end;
-
-function RandomMax(const x: Integer; const r: Integer = 2): Integer; overload; 
-var
-  i: Integer;
-begin
-  Result := -2147483648;
-  for i := 1 to r do
-    Result := Max(Result, Random(x));
-end;
-
-function RandomMin(const x: Integer; const r: Integer = 2): Integer; overload; 
-var
-  i: Integer;
-begin
-  Result := 2147483647;
-  for i := 1 to r do
-    Result := Min(Result, Random(x));
-end;
-
-function Swop(var A, B: Integer; const oAscending: Boolean = True): Boolean; overload; begin Result := specialize Swop<Integer>(A, B, oAscending); end;
-function Swop(var A, B: Int64; const oAscending: Boolean = True): Boolean; overload; begin Result := specialize Swop<Int64>(A, B, oAscending); end;
-function Swop(var A, B: Double; const oAscending: Boolean = True): Boolean; overload; begin Result := specialize Swop<Double>(A, B, oAscending); end;
-function Swop(var A, B: string; const oAscending: Boolean = True): Boolean; overload; begin Result := specialize Swop<string>(A, B, oAscending); end;
-function Swop(var A, B: Char; const oAscending: Boolean = True): Boolean; overload; begin Result := specialize Swop<Char>(A, B, oAscending); end;
-function Swop(var A, B: TPoint; const oAscending: Boolean = True): Boolean; overload; begin Result := specialize Swop<TPoint>(A, B, oAscending); end;
-function Swop(var A, B, C: Integer; const oAscending: Boolean = True): Boolean; overload; begin Result := specialize Swop<Integer>(A, B, C, oAscending); end;
-function Swop(var A, B, C: Int64; const oAscending: Boolean = True): Boolean; overload; begin Result := specialize Swop<Int64>(A, B, C, oAscending); end;
-function Swop(var A, B, C: Double; const oAscending: Boolean = True): Boolean; overload; begin Result := specialize Swop<Double>(A, B, C, oAscending); end;
-function Swop(var A, B, C: string; const oAscending: Boolean = True): Boolean; overload; begin Result := specialize Swop<string>(A, B, C, oAscending); end;
-function Swop(var A, B, C: Char; const oAscending: Boolean = True): Boolean; overload; begin Result := specialize Swop<Char>(A, B, C, oAscending); end;
-function Swop(var A, B, C: TPoint; const oAscending: Boolean = True): Boolean; overload; begin Result := specialize Swop<TPoint>(A, B, C, oAscending); end;
-
-function Sort(var A, B: Integer; const oAscending: Boolean = True): Boolean; overload; begin Result := specialize Sort<Integer>(A, B, oAscending); end;
-function Sort(var A, B: Int64; const oAscending: Boolean = True): Boolean; overload; begin Result := specialize Sort<Int64>(A, B, oAscending); end;
-function Sort(var A, B: Double; const oAscending: Boolean = True): Boolean; overload; begin Result := specialize Sort<Double>(A, B, oAscending); end;
-function Sort(var A, B: string; const oAscending: Boolean = True): Boolean; overload; begin Result := specialize Sort<string>(A, B, oAscending); end;
-function Sort(var A, B: Char; const oAscending: Boolean = True): Boolean; overload; begin Result := specialize Sort<Char>(A, B, oAscending); end;
-function Sort(var A, B: TPoint; const oAscending: Boolean = True): Boolean; overload; begin Result := specialize Sort<TPoint>(A, B, oAscending); end;
-function Sort(var A, B, C: Integer; const oAscending: Boolean = True): Boolean; overload; begin Result := specialize Sort<Integer>(A, B, C, oAscending); end;
-function Sort(var A, B, C: Int64; const oAscending: Boolean = True): Boolean; overload; begin Result := specialize Sort<Int64>(A, B, C, oAscending); end;
-function Sort(var A, B, C: Double; const oAscending: Boolean = True): Boolean; overload; begin Result := specialize Sort<Double>(A, B, C, oAscending); end;
-function Sort(var A, B, C: string; const oAscending: Boolean = True): Boolean; overload; begin Result := specialize Sort<string>(A, B, C, oAscending); end;
-function Sort(var A, B, C: Char; const oAscending: Boolean = True): Boolean; overload; begin Result := specialize Sort<Char>(A, B, C, oAscending); end;
-function Sort(var A, B, C: TPoint; const oAscending: Boolean = True): Boolean; overload; begin Result := specialize Sort<TPoint>(A, B, C, oAscending); end;
-
-{$mode objfpc}{$H+}
 
 {$I MiMU\MiMU.inc}
 
